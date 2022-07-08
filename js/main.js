9ba9bf7021d658f2a1614543c8421fff329cca40
EmirAkimzhanov <emir.akimzhanov@gmail.com>
2022-07-08 16:12:16 +0600

    navbar js added

---
+++ js/main.js
@@ -0,0 +1,21 @@
+window.onscroll = function () {
+  scrollFunction();
+};
+const myNav = document.getElementById(".bg-dark");
+const navBrand = document.getElementById("navBrand");
+const navLink = document.getElementsByClassName("nav-link");
+function scrollFunction() {
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+    myNav.classList.add("scrolled");
+    navBrand.classList.add("scrolled-txt");
+    for (let i = 0; i < navLink.length; i++) {
+      navLink[i].classList.add("scrolled-txt");
+    }
+  } else {
+    myNav.classList.remove("scrolled");
+    navBrand.classList.remove("scrolled-txt");
+    for (let i = 0; i < navLink.length; i++) {
+      navLink[i].classList.remove("scrolled-txt");
+    }
+  }
+}
