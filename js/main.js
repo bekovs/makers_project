7d44281664699bff5ea8fd389fe892d181612930
EmirAkimzhanov <emir.akimzhanov@gmail.com>
2022-07-08 16:33:03 +0600

    navbar fixed

--- js/main.js
+++ js/main.js
@@ -5,7 +5,10 @@ const myNav = document.getElementById(".bg-dark");
 const navBrand = document.getElementById("navBrand");
 const navLink = document.getElementsByClassName("nav-link");
 function scrollFunction() {
-  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+  if (
+    document.body.scrollTop > 730 ||
+    document.documentElement.scrollTop > 730
+  ) {
     myNav.classList.add("scrolled");
     navBrand.classList.add("scrolled-txt");
     for (let i = 0; i < navLink.length; i++) {
